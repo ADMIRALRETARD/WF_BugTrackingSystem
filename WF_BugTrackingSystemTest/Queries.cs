a53ac8a4feed000f69793886958bba789539a759
Kirill Kalyonov <buoiiiok@yahoo.com>
2018-09-25 22:04:01 +0300

    small fixes

--- WF_BugTrackingSystemTest/Queries.cs
+++ WF_BugTrackingSystemTest/Queries.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WF_BugTrackingSystemTest
+﻿namespace WF_BugTrackingSystemTest
 {
     class Queries
     {
        public static string showUsers = "select * from users";
        public static string showUsers2 = "select ID,LastName from users";
        public static string showProjects = "select *from projects";
-        public static string showTasks = "select Tasks.ID,Projects.ProjectName,Theme,Type,Priority,Users.LastName,Description " +
+       public static string showTasks = "select Tasks.ID,Projects.ProjectName,Theme,Type,Priority,Users.LastName,Description " +
                                          "from Tasks inner  join Projects on Tasks.ProjectID = Projects.ID " +
                                          "inner join Users on Tasks.UserID = Users.ID";
-
-
-
+        
     }
 }
